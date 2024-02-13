7394c43ed6ee66c9561140993153cb6e2e855d16
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2024-02-13 17:55:01 +0530

    basic structure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center bg-green-500 h-screen">
+      <h1 className="text-orange-100">shiva</h1>
+    </div>
+  );
+}
+
+export default App;
