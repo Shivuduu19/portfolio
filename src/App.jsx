496c8d792ac5e74e698b7a5dd5ea18fa0ccdd1ae
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2024-02-15 22:54:10 +0530

    add projects section

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import shoppingCart from "./assets/shoppingCart.png";
+
 function App() {
   return (
     <>
@@ -25,7 +27,7 @@ function App() {
           </div>
         </div>
       </header>
-      <main className="flex">
+      <main>
         {/* intro section */}
         <section className="mx-auto">
           <div className="container m-auto px-4 py-10">
@@ -36,11 +38,131 @@ function App() {
               <h1 className="font-bold text-3xl text-center clip mt-1">
                 Front-end developer
               </h1>
-              <p className="text-center mt-5 text-gray-400 ">
+              <p className="mx-auto text-center  mt-5 text-gray-400 w-2/4">
                 I have 1 year of experience in creating and developing user
                 interfaces and I'm seeking to apply and develop my skills
                 further
               </p>
+            </div>
+          </div>
+        </section>
+        {/* projects section */}
+        <section>
+          <div className="container m-auto">
+            <h1 className="text-3xl font-bold">Projects</h1>
+            <div className="flex justify-center gap-7 mt-5">
+              <div className="w-1/3 border-blue-600 border rounded-md p-6">
+                <img src={shoppingCart} />
+                <h2 className="font-semibold text-sm mt-4">
+                  fake shopping cart for shopping things in online
+                </h2>
+                <p className="text-xs mt-2 break-words">
+                  Responsive page made with reactjs, redux,html5,css3,fakeAPI
+                  and styled-components
+                </p>
+                <div className="flex gap-4 mt-5 justify-center">
+                  <button className="text-xs lg:text-lg clip from-white to-yellow-400 hover:from-yellow-500">
+                    source code
+                  </button>
+                  <button className="text-xs bg-gradient-to-r from-red-300 to-green-500 rounded-md p-3 hover:from-red-500 ">
+                    Live Preview
+                  </button>
+                </div>
+              </div>
+              <div className="w-1/3 border-blue-600 border rounded-md p-6">
+                <img src={shoppingCart} />
+                <h2 className="font-semibold text-sm mt-4">
+                  fake shopping cart for shopping things in online
+                </h2>
+                <p className="text-xs mt-2 break-words">
+                  Responsive page made with reactjs, redux,html5,css3,fakeAPI
+                  and styled-components
+                </p>
+                <div className="flex gap-4 mt-5 justify-center">
+                  <button className="text-xs lg:text-lg clip from-white to-yellow-400 hover:from-yellow-500">
+                    source code
+                  </button>
+                  <button className="text-xs bg-gradient-to-r from-red-300 to-green-500 rounded-md p-3 hover:from-red-500 ">
+                    Live Preview
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-center gap-7 mt-5">
+              <div className="w-1/3 border-blue-600 border rounded-md p-6">
+                <img src={shoppingCart} />
+                <h2 className="font-semibold text-sm mt-4">
+                  fake shopping cart for shopping things in online
+                </h2>
+                <p className="text-xs mt-2 break-words">
+                  Responsive page made with reactjs, redux,html5,css3,fakeAPI
+                  and styled-components
+                </p>
+                <div className="flex gap-4 mt-5 justify-center">
+                  <button className="text-xs lg:text-lg clip from-white to-yellow-400 hover:from-yellow-500">
+                    source code
+                  </button>
+                  <button className="text-xs bg-gradient-to-r from-red-300 to-green-500 rounded-md p-3 hover:from-red-500 ">
+                    Live Preview
+                  </button>
+                </div>
+              </div>
+              <div className="w-1/3 border-blue-600 border rounded-md p-6">
+                <img src={shoppingCart} />
+                <h2 className="font-semibold text-sm mt-4">
+                  fake shopping cart for shopping things in online
+                </h2>
+                <p className="text-xs mt-2 break-words">
+                  Responsive page made with reactjs, redux,html5,css3,fakeAPI
+                  and styled-components
+                </p>
+                <div className="flex gap-4 mt-5 justify-center">
+                  <button className="text-xs lg:text-lg clip from-white to-yellow-400 hover:from-yellow-500">
+                    source code
+                  </button>
+                  <button className="text-xs bg-gradient-to-r from-red-300 to-green-500 rounded-md p-3 hover:from-red-500 ">
+                    Live Preview
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-center gap-7 mt-5">
+              <div className="w-1/3 border-blue-600 border rounded-md p-6">
+                <img src={shoppingCart} />
+                <h2 className="font-semibold text-sm mt-4">
+                  fake shopping cart for shopping things in online
+                </h2>
+                <p className="text-xs mt-2 break-words">
+                  Responsive page made with reactjs, redux,html5,css3,fakeAPI
+                  and styled-components
+                </p>
+                <div className="flex gap-4 mt-5 justify-center">
+                  <button className="text-xs lg:text-lg clip from-white to-yellow-400 hover:from-yellow-500">
+                    source code
+                  </button>
+                  <button className="text-xs bg-gradient-to-r from-red-300 to-green-500 rounded-md p-3 hover:from-red-500 ">
+                    Live Preview
+                  </button>
+                </div>
+              </div>
+              <div className="w-1/3 border-blue-600 border rounded-md p-6">
+                <img src={shoppingCart} />
+                <h2 className="font-semibold text-sm mt-4">
+                  fake shopping cart for shopping things in online
+                </h2>
+                <p className="text-xs mt-2 break-words">
+                  Responsive page made with reactjs, redux,html5,css3,fakeAPI
+                  and styled-components
+                </p>
+                <div className="flex gap-4 mt-5 justify-center">
+                  <button className="text-xs lg:text-lg clip from-white to-yellow-400 hover:from-yellow-500">
+                    source code
+                  </button>
+                  <button className="text-xs bg-gradient-to-r from-red-300 to-green-500 rounded-md p-3 hover:from-red-500 ">
+                    Live Preview
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </section>
